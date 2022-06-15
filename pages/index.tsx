ee5291b3bb582f4b2826195b3809210608e16f75
Stanislav Ussanov <s.ussanov@mail.ru>
2022-06-16 00:04:34 +0600

    добавить mobx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,20 @@ import type { NextPage } from 'next';
 // @ts-ignore
 import styles from '../styles/home.module.less';
 import { Button } from 'antd';
+import { observer } from 'mobx-react-lite';
+import { appStore } from '../store/app-store';
 
-const Home: NextPage = () => {
+const Home: NextPage = observer(() => {
   return (
     <div className={styles.container}>
-      <Button type="primary">
+      <Button
+        type="primary"
+        onClick={() => appStore.testMobx()}
+      >
         Hello!
       </Button>
     </div>
   );
-};
+});
 
 export default Home;
